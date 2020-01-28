0984324063d25132f1bdc5f22fde36b8d3e75f19
cay18002@byui.edu <cay18002@byui.edu>
2020-01-28 13:54:37 +0900

    Initial Commit

---
+++ lesson4/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() 
+{
+
+    document.getElementById("mainNav").classList.toggle("navigationLists");
+}
+
+var x = document.lastModified;
+document.getElementById("lastDate").innerHTML = x;
+
