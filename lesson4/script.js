e3d120261d821866575da476190353354afa0568
cay18002@byui.edu <cay18002@byui.edu>
2020-01-28 14:26:45 +0900

    sad

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,9 +1,7 @@
-function toggleMenu() 
-{
+function toggleMenu() {
 
     document.getElementById("mainNav").classList.toggle("navigationLists");
 }
 
 var x = document.lastModified;
 document.getElementById("lastDate").innerHTML = x;
-
