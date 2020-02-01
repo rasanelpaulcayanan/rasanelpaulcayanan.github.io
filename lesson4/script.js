5cd89c9ff6e42af2d754197fa53e2dc338eabce2
cay18002@byui.edu <cay18002@byui.edu>
2020-02-01 10:23:41 +0900

    Initial Commit

--- lesson4/script.js
+++ lesson4/script.js
@@ -3,5 +3,7 @@ function toggleMenu() {
     document.getElementById("mainNav").classList.toggle("navigationLists");
 }
 
-var x = document.lastModified;
-document.getElementById("lastDate").innerHTML = x;
+
+
+var dateNow = document.dateNow;
+document.getElementById("dateNow").innerHTML=new Date().toLocaleDateString(navigator.language,{weekday:'long',year:'numeric',month:'long',day:'numeric'});
