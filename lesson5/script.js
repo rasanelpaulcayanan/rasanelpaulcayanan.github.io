4e0aa93ac3a1817b92cfc3b43d3c1c584c33663c
cay18002@byui.edu <cay18002@byui.edu>
2020-02-03 12:29:18 +0900

    test

---
+++ lesson5/script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+
+    document.getElementById("mainNav").classList.toggle("navigationLists");
+}
+
+
+
+var dateNow = document.dateNow;
+document.getElementById("dateNow").innerHTML = new Date().toLocaleDateString(navigator.language, {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
