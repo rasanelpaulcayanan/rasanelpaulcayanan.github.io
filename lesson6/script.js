38f63bab2a5ea84e7349ae1c61da3addd34857e5
cay18002@byui.edu <cay18002@byui.edu>
2020-02-10 14:06:08 +0900

    changed images

--- lesson6/script.js
+++ lesson6/script.js
@@ -20,6 +20,12 @@ function toggleMenu() {
 
 
 
+
+
+
+
+
+
 var dateNow = document.dateNow;
 document.getElementById("dateNow").innerHTML = new Date().toLocaleDateString(navigator.language, {
     weekday: 'long',
