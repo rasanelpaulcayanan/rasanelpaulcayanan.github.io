0c62d267feeffa2c44f00361863356dcb2aa04db
cay18002@byui.edu <cay18002@byui.edu>
2020-02-10 12:24:47 +0900

    changes

---
+++ lesson6/script.js
@@ -0,0 +1,29 @@
+window.onload = function showAnnouncement(){
+    var announcement = document.getElementById("announcementDiv");
+    var today = new Date();
+    var day = today.getDay();
+
+    if (day == 5) {
+    announcement.style.display ="block";
+    }
+    
+    else {
+    announcement.style.display="none";
+    
+    }
+}
+
+function toggleMenu() {
+
+    document.getElementById("mainNav").classList.toggle("navigationLists");
+}
+
+
+
+var dateNow = document.dateNow;
+document.getElementById("dateNow").innerHTML = new Date().toLocaleDateString(navigator.language, {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
