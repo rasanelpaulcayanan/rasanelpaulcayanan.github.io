2ea4fd98b97ffef68b04ea95abb41bfb2b17fb1b
cay18002@byui.edu <cay18002@byui.edu>
2020-02-28 11:51:17 +0900

    Javascript fixed

--- lesson8/script/script.js
+++ lesson8/script/script.js
@@ -38,3 +38,17 @@ if ('IntersectionObserver' in window) {
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+function toggleMenu() {
+
+    document.getElementById("mainNav").classList.toggle("navigationLists");
+}
+
+
+var dateNow = document.dateNow;
+document.getElementById("dateNow").innerHTML = new Date().toLocaleDateString(navigator.language, {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
