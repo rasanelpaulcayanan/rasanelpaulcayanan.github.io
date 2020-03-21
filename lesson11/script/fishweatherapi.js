3cc9c01c0b0a6428baf7992f367d2687cbf9859d
cay18002@byui.edu <cay18002@byui.edu>
2020-03-21 11:35:40 +0900

    sasd

--- lesson11/script/fishweatherapi.js
+++ lesson11/script/fishweatherapi.js
@@ -11,5 +11,22 @@ fetch(apiURL)
     document.getElementById('windspeed').textContent = jsObject.wind.speed;
 
     
+    let temp = parseFloat(document.getElementById('temperature').textContent);
+    let speed = parseFloat(document.getElementById('windspeed').textContent);
+
+
+    if (temp<=50 && speed>=3) {
+
+        windchill = 35.74 + (0.6215 * temp) - (35.75 * (Math.pow(speed, 0.16))) + (0.425 * temp * (Math.pow(speed, 0.16)));
+    
+        document.getElementById('windChill').textContent = (windchill).toFixed(2);
+    }
+    
+    else {
+    
+        document.getElementById('windChill').textContent = ("N/A");
+    }
+    
+
   });
 
